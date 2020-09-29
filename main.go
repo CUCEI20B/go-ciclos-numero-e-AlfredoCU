f2c4e64f187f33dbc205432419b36e2ffeaf601f
AlfredoCU <alfredo_11_cu@hotmail.com>
2020-09-29 13:50:21 -0500

    Códig para calcular el numero de e

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	// "math"
+)
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	// Declaration of variable
+	var e float64
+	var f float64 = 1
+	var limit int
+
+	// Input
+	fmt.Scan(&limit)
+
+	// Calculation of Number e with factorial
+	for i := 0; i < limit; i++ { // 17 Iterations for ê
+		f = 1
+		for j := i; j > 1; j-- {
+			f *= float64(j)
+		}
+		e += 1 / f
+	}
+
+	// Calculation of Number e
+	// for n := 0; n <= 1000000; n++ {
+	//		e = math.Pow(1 + (1 / float64(n)), float64(n))
+	// }
+
+	// Print result
+	fmt.Print(e)
+	// ê = 2.7182818284590455 // One method
+	// ê = 2.7182804690957534 // Two method
+	// e = 2.718281828459045
 }
